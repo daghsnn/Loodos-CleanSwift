9c8a20aa4744f4f21e414966f9fcf873ae61fcc7
Hasan Dağ <daghsnn@gmail.com>
2022-09-25 19:08:10 +0300

    Initial Commit

---
+++ Loodos-CaseStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loodos-CaseStudy
+//
+//  Created by Hasan Dag on 25.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
